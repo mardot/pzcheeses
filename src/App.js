57832f565d44e7cfe3ed9e7b12f629dec835fe43
mitchell-elliott <mitchell_elliott@live.com.au>
2021-08-13 15:02:58 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import clsx from "clsx";
+import {
+  Button,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  CssBaseline,
+  Collapse,
+  IconButton,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+//import "./App.css";
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 function App() {
+  // store cheese object form API
+  const [cheeses, setCheeses] = useState([]);
+
+  // calculator drop down on card component
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const [inputWeight, setInputWeight] = useState(0);
+
+  // Get JSON only once on load
+  // Would upgrade this to use restful API (Async/await)
+  useEffect(() => {
+    axios.get("http://localhost:3001/cheeses").then((res) => {
+      setCheeses(res.data);
+    });
+  }, []);
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            PZ Cheeses
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              PZ Cheeses
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cheeses.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.image}
+                    title={card.title}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography>{card.description}</Typography>
+                    <Typography variant="h6">
+                      Price per Kg ${card.price}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <IconButton
+                      className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                      })}
+                      onClick={handleExpandClick}
+                      aria-expanded={expanded}
+                      aria-label="show more"
+                    >
+                      Calculate Price
+                      <ExpandMoreIcon />
+                    </IconButton>
+                  </CardActions>
+                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                      <Typography>
+                        Calculate the price for your order
+                      </Typography>
+                      <Typography color="primary">
+                        ${card.price} kg x
+                        <input type="weight" value={inputWeight} />={" "}
+                        {card.price * inputWeight}
+                      </Typography>
+                    </CardContent>
+                  </Collapse>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          PZ Cheeses
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
         >
-          Learn React
-        </a>
-      </header>
+          Copyright Mitchell Elliott 2021
+        </Typography>
+      </footer>
+      {/* End footer */}
     </div>
   );
 }
